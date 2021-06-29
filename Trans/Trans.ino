5a407e043d9e1b729651f22c3f46da55f4f81b85
Kubera  Murthi <70520314+kuberan0810@users.noreply.github.com>
2021-06-29 19:27:23 +0530

    Add files via upload

---
+++ Trans/Trans.ino
@@ -0,0 +1,20 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+void setup() 
+{
+  pinMode(6,OUTPUT);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() 
+{
+  digitalWrite(6,1);
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
